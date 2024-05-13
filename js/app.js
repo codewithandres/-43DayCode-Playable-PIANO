a761191eb5d6084e148af853c63c46d79a65c0c5
Dize dev <axiozweb2022@gmail.com>
2024-05-12 22:56:02 -0500

    agrega y muestra una clase activa

--- js/app.js
+++ js/app.js
@@ -4,10 +4,23 @@ const pianoKey = document.querySelectorAll('.piano-keys .key');
 let audio = new Audio('../tunes/a.wav');
 
 const playTune = (key) => {
+
     audio.src = `../tunes/${key}.wav`;
     audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add('active');
+
+    setTimeout(() => clickedKey.classList.remove('active'), 150);
 };
 
 [...pianoKey].map(key => {
     key.addEventListener('click', () => playTune(key.dataset.key));
 });
+
+const presetKey = (event) => {
+    const { key } = event;
+    playTune(key);
+};
+
+document.addEventListener('keydown', presetKey);
